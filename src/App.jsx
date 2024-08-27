895cab4c316e6ffb1fa17a8db10a66b3c5a5394d
Sohail <msksohailk@gmail.com>
2024-08-27 19:01:54 +0530

    React project structure added

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Homepage from './components/HomePage';
+
+function App() {
+  return (
+ <Homepage/>
+  )
+}
+    export default App
---
+++ src/components/HomePage.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div>
+        <ul>
+        <p>Cars</p>
+            <p>Properties</p>
+            <li>For Sale: Houses & Apartments</li>
+            <li>For Rent: Houses & Apartments</li>
+            <li>Lands & Plots</li>
+            <li>For Rent: Shops & Offices</li>
+            <li>PG & Guest Houses</li>
+        </ul>
+
+        <ul>
+        <p>Mobile Phones</p>
+            <li>Mobile Phones</li>
+            <li>Accessories</li>
+            <li>Tablets</li>
+            
+        </ul>
+
+        <ul>
+        <p>Mobile Phones</p>
+            <li>Mobile Phones</li>
+            <li>Accessories</li>
+            <li>Tablets</li>
+            
+        </ul>
+
+        
+      
+    </div>
+  )
+}
+
+export default HomePage
